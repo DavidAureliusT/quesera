bf4764520cb6fb531bac5f5992c6b371b87153b5
DavidAureliusT <vidtandjung@gmail.com>
2025-06-12 14:19:19 +0700

    feat: project_key autofill

--- resources/js/pages/projects/Create.vue
+++ resources/js/pages/projects/Create.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { Head, useForm } from '@inertiajs/vue3';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
@@ -7,16 +6,60 @@ import { LoaderCircle } from 'lucide-vue-next';
 import InputError from '@/components/InputError.vue';
 import SandboxLayout from '@/layouts/SandboxLayout.vue';
 
+import { Head, useForm } from '@inertiajs/vue3';
+import { watch, ref } from 'vue';
+
 const form = useForm({
     project_name: '',
     project_key: '',
 });
 
-// const autofill_handler = () => { };
+const userEditedProjectKey = ref(false);
+
+let projectNameTimer: ReturnType<typeof setTimeout> | null = null;
+
+function getAutoFillProjectKey(project_name: string): string {
+    let project_key = "";
+
+    const initial_name = project_name.trim().split(' ');
+    initial_name.forEach((word) => {
+        if (word != undefined) project_key += word[0];
+    });
+
+    if (project_key.length === 1) {
+        project_key = initial_name[0].slice(0, 3);
+    }
+    return project_key.toUpperCase();
+}
+
+watch(() => form.project_key, (newProjectKey) => {
+    userEditedProjectKey.value = newProjectKey !== getAutoFillProjectKey(form.project_name);
+});
+
+watch(() => form.project_name, (newProjectName) => {
+    if (projectNameTimer) {
+        clearTimeout(projectNameTimer);
+    }
+    if (newProjectName) {
+        projectNameTimer = setTimeout(() => {
+            if (!userEditedProjectKey.value) {
+                form.project_key = getAutoFillProjectKey(newProjectName);
+            }
+            if (!form.project_key && newProjectName) {
+                form.project_key = getAutoFillProjectKey(newProjectName);
+            }
+        }, 1000);
+    } else {
+        if (!userEditedProjectKey.value) {
+            form.project_key = '';
+        }
+    }
+});
 
 const submit = () => {
     form.post(route('projects.store'));
 };
+
 </script>
 
 <template>
@@ -35,7 +78,7 @@ const submit = () => {
                 </div>
                 <div class="flex flex-col gap-[1.2em] pt-[.8em] pb-[1.2em]">
                     <div class="gap-2 grid">
-                        <Label for="project_name">Name<span aria-hidden="true" title="required" class="text-orange-600">*</span></Label>
+                        <Label for="project_name">Name {{ form.project_name.trim().split(' ').length }}<span aria-hidden="true" title="required" class="text-orange-600">*</span></Label>
                         <Input id="project_name" class="" type="text" required autofocus :tabindex="1" autocomplete="project_name" v-model="form.project_name" placeholder="Try a team name, project goal, milestone..." />
                         <InputError :message="form.errors.project_name" />
                     </div>
