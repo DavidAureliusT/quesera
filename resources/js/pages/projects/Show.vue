38a4f2c10aa56de8e273c86cafbc742667a22293
daurelius <davad.tandjung@gmail.com>
2025-06-17 00:52:41 +0700

    chore: set badge color on each workflow category

--- resources/js/pages/projects/Show.vue
+++ resources/js/pages/projects/Show.vue
@@ -4,16 +4,7 @@ import { Head } from '@inertiajs/vue3';
 import TextLink from "@/components/TextLink.vue";
 import SandboxLayout from '@/layouts/SandboxLayout.vue';
 import { Badge } from '@/components/ui/badge';
-
-import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from '@/components/ui/table';
+import Tasks from "./Tasks.vue";
 
 interface Props {
     project: Project
@@ -46,47 +37,16 @@ const createTask: NavItem = {
                 <Badge class="uppercase">{{ project.type }} Workflow</Badge>
                 <div class="flex gap-2 mt-4">
                     <div v-for="(status, index) in project.workflows" :key="status.id">
-                        <span class="pr-2 uppercase">
-                            <Badge v-if="status.category == 'To do'" variant="outline">{{ status.name }}</Badge>
-                            <Badge v-if="status.category == 'In Progress'" variant="secondary">{{ status.name }}</Badge>
-                            <Badge v-if="status.category == 'Done'" variant="destructive">{{ status.name }}</Badge>
+                        <span class="pr-2">
+                            <Badge v-if="status.category == 'To do'" variant="secondary">{{ status.name }}</Badge>
+                            <Badge v-if="status.category == 'In Progress'" variant="secondary" class="bg-blue-800">{{ status.name }}</Badge>
+                            <Badge v-if="status.category == 'Done'" variant="secondary" class="bg-green-800">{{ status.name }}</Badge>
                         </span> <span v-if="index < project.workflows!.length - 1"> -> </span>
                     </div>
                 </div>
                 <h1>{{ project.key }} - {{ project.name }}</h1>
             </div>
-            <Table>
-                <TableCaption>A list of your tasks.</TableCaption>
-                <TableHeader>
-                    <TableRow>
-                        <TableHead class="w-[100px]">
-                            Key
-                        </TableHead>
-                        <TableHead>Summary</TableHead>
-                        <TableHead class="text-right">
-                            Point
-                        </TableHead>
-                        <TableHead>Status</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    <TableRow v-for="task in project.tasks" :key="task.key">
-                        <TableCell class="font-medium">
-                            {{ task.key }}
-                        </TableCell>
-                        <TableCell>{{ task.summary }}</TableCell>
-                        <TableCell class="text-right">
-                            {{ task.point }}
-                        </TableCell>
-                        <TableCell>
-                            <Badge v-if="task.status_category == 'To do'" variant="outline">{{ task.status_name }}</Badge>
-                            <Badge v-if="task.status_category == 'In Progress'" variant="secondary">{{ task.status_name }}</Badge>
-                            <Badge v-if="task.status_category == 'Done'" variant="destructive">{{ task.status_name }}</Badge>
-                        </TableCell>
-                    </TableRow>
-                </TableBody>
-            </Table>
-            <!-- <pre>{{ props }}</pre> -->
+            <Tasks :tasks="project.tasks!"></Tasks>
         </main>
     </SandboxLayout>
 </template>
---
+++ resources/js/pages/projects/Tasks.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { Task } from '@/types';
+
+import { Badge } from '@/components/ui/badge';
+
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from '@/components/ui/table';
+
+interface Props {
+    tasks: Task[]
+}
+
+defineProps<Props>()
+
+
+</script>
+
+<template>
+    <Table>
+        <TableCaption>A list of your tasks.</TableCaption>
+        <TableHeader>
+            <TableRow>
+                <TableHead class="w-[100px]">
+                    Key
+                </TableHead>
+                <TableHead>Summary</TableHead>
+                <TableHead class="text-right">
+                    Point
+                </TableHead>
+                <TableHead>Status</TableHead>
+            </TableRow>
+        </TableHeader>
+        <TableBody>
+            <TableRow v-for="task in tasks" :key="task.key">
+                <TableCell class="font-medium">
+                    {{ task.key }}
+                </TableCell>
+                <TableCell>{{ task.summary }}</TableCell>
+                <TableCell class="text-right">
+                    {{ task.point }}
+                </TableCell>
+                <TableCell>
+                    <Badge v-if="task.status_category == 'To do'" variant="secondary">{{ task.status_name }}</Badge>
+                    <Badge v-if="task.status_category == 'In Progress'" variant="secondary" class="bg-blue-800">{{ task.status_name }}</Badge>
+                    <Badge v-if="task.status_category == 'Done'" variant="secondary" class="bg-green-800">{{ task.status_name }}</Badge>
+                </TableCell>
+            </TableRow>
+        </TableBody>
+    </Table>
+</template>
