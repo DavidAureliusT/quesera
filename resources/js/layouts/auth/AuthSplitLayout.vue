8330e0df547e70d11e7a88b82dbd4fb302bec569
DavidAureliusT <vidtandjung@gmail.com>
2025-07-25 16:07:47 +0700

    split login

--- resources/js/layouts/auth/AuthSplitLayout.vue
+++ resources/js/layouts/auth/AuthSplitLayout.vue
@@ -13,25 +13,26 @@ defineProps<{
 </script>
 
 <template>
-    <div class="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
-        <div class="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
-            <div class="absolute inset-0 bg-zinc-900" />
-            <Link :href="route('home')" class="relative z-20 flex items-center text-lg font-medium">
-                <AppLogoIcon class="mr-2 size-8 fill-current text-white" />
-                {{ name }}
+    <div class="relative flex-col justify-center items-center grid lg:grid-cols-2 px-8 sm:px-0 lg:px-0 lg:max-w-none h-dvh">
+        <div class="hidden relative lg:flex flex-col bg-black p-10 dark:border-r h-full text-white">
+            <div class="absolute inset-0 bg-[url(http://127.0.0.1:8000/assets/bg-login.jpg)] bg-cover blur-[.1em] brightness-50 grayscale" />
+            <div class="absolute inset-0 bg-primary/40" />
+            <Link :href="route('home')" class="z-20 relative flex items-center font-medium text-lg">
+            <AppLogoIcon class="fill-current mr-2 size-8 text-white" />
+            {{ name }}
             </Link>
-            <div v-if="quote" class="relative z-20 mt-auto">
+            <div v-if="quote" class="z-20 relative mt-auto">
                 <blockquote class="space-y-2">
                     <p class="text-lg">&ldquo;{{ quote.message }}&rdquo;</p>
-                    <footer class="text-sm text-neutral-300">{{ quote.author }}</footer>
+                    <footer class="text-neutral-300 text-sm">{{ quote.author }}</footer>
                 </blockquote>
             </div>
         </div>
         <div class="lg:p-8">
-            <div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+            <div class="flex flex-col justify-center space-y-6 mx-auto w-full sm:w-[350px]">
                 <div class="flex flex-col space-y-2 text-center">
-                    <h1 class="text-xl font-medium tracking-tight" v-if="title">{{ title }}</h1>
-                    <p class="text-sm text-muted-foreground" v-if="description">{{ description }}</p>
+                    <h1 class="font-medium text-xl tracking-tight" v-if="title">{{ title }}</h1>
+                    <p class="text-muted-foreground text-sm" v-if="description">{{ description }}</p>
                 </div>
                 <slot />
             </div>
